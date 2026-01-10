e088e0a33ea675db662136aa009ebb62071cdab3
AshutoshPandey <ashutosh2239@gmail.com>
2026-01-11 03:01:38 +0530

    error middleware fix

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -9,7 +9,6 @@ import { respond } from "../utils/respond.js";
 import { HTTP_CODES } from "../utils/httpCodes.js";
 import { SERVER_MESSAGES } from "../utils/messages/server.message.js";
 import { ZodError } from "zod";
-import { env } from "../config/env.js";
 
 export const globalErrorHandler = (err, req, res, next) => {
   if (err instanceof ZodError) {
@@ -25,14 +24,9 @@ export const globalErrorHandler = (err, req, res, next) => {
     });
   }
 
-  const isProduction = env.NODE_ENV === "production";
-
   const status = err.statusCode || HTTP_CODES.INTERNAL_SERVER_ERROR;
-
-  const message = isProduction
-    ? SERVER_MESSAGES.INTERNAL_SERVER_ERROR
-    : err.message || SERVER_MESSAGES.INTERNAL_SERVER_ERROR;
-  const errors = isProduction ? [] : err.errors || [];
+  const message = err.message || SERVER_MESSAGES.INTERNAL_SERVER_ERROR;
+  const errors = err.errors || [];
 
   return respond.fail(res, { statusCode: status, message, errors });
 };
