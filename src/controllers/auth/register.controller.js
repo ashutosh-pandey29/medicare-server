a6f66e6ad3fabaa0f9761379d9a89b36f6fa3f0d
AshutoshPandey <ashutosh2239@gmail.com>
2026-01-11 02:53:40 +0530

    fixing register bug

--- src/controllers/auth/register.controller.js
+++ src/controllers/auth/register.controller.js
@@ -16,10 +16,9 @@ import { AUTH_MESSAGES } from "../../utils/messages/auth.message.js";
 export const registerController = async (req, res, next) => {
   try {
     const response  =  await registerService(req.body);
-
     respond.success(res, response  );
   } catch (err) {
-    // console.log(err);
+    console.log("err register " , err);
     next(err);
   }
 };
