f91017eea35c30ba4aae5f0575ca1c9979273667
AshutoshPandey <ashutosh2239@gmail.com>
2026-02-11 23:43:03 +0530

    comment added in webpush config

--- src/config/webpush.config.js
+++ src/config/webpush.config.js
@@ -1,6 +1,17 @@
+// Import web-push library to send push notifications
 import webpush from "web-push";
+
+// Import environment variables (VAPID keys)
 import { env } from "./env.js";
+
+/**
+ * Configure VAPID details for Web Push
+ *
+ * VAPID = Voluntary Application Server Identification
+ * It is used to identify your server when sending push notifications.
+ */
 
 webpush.setVapidDetails(env.VAPID_SUBJECT, env.VAPID_PUBLIC_KEY, env.VAPID_PRIVATE_KEY);
 
+// Export configured webpush instance
 export default webpush;
