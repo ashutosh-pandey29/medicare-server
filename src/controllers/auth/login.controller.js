af932c04ebc310f03113b8269e9d5a3a58c806b3
AshutoshPandey <ashutosh2239@gmail.com>
2026-02-11 23:12:46 +0530

    login controller updated

--- src/controllers/auth/login.controller.js
+++ src/controllers/auth/login.controller.js
@@ -1,22 +1,24 @@
 /**
  * Purpose:
  * Handles user login using email and password.
- *
+ 
  * When it is used:
  * Called when a user tries to log in with custom credentials.
- *
+ 
  * Access:
  * Public route (no authentication required).
  */
 
+// Import required modules and helpers
 import { loginService } from "../../services/auth/login.service.js";
 import { setCookie } from "../../utils/cookie.js";
 import { HTTP_CODES } from "../../utils/httpCodes.js";
 import { AUTH_MESSAGES } from "../../utils/messages/auth.message.js";
 import { respond } from "../../utils/respond.js";
-
+ 
 export const loginController = async (req, res, next) => {
   try {
+
     // Call the login service to validate user and get tokens
     const { accessToken, refreshToken } = await loginService(req.body);
 
@@ -25,12 +27,13 @@ export const loginController = async (req, res, next) => {
     setCookie(res, refreshToken);
 
     // Send success response with access token
-
     respond.success(res, {
       statusCode: HTTP_CODES.OK,
       message: AUTH_MESSAGES.LOGIN_SUCCESS,
       data: { accessToken},
     });
+
+
   } catch (err) {
     // pass error to global error handler
     next(err);
