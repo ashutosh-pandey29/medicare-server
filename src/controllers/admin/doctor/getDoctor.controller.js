821244ba506bfccfacb73eaba4cef7528fe9b650
AshutoshPandey <ashutosh2239@gmail.com>
2026-02-12 00:01:15 +0530

    controller updated

--- src/controllers/admin/doctor/getDoctor.controller.js
+++ src/controllers/admin/doctor/getDoctor.controller.js
@@ -1,13 +1,16 @@
+// Import service layer function to fetch doctors
 import { getDoctorService } from "../../../services/admin/getDoctor.service.js";
+
+// Import standardized response handler
 import { respond } from "../../../utils/respond.js";
 
 export const getDoctorController = async (req, res, next) => {
   try {
- 
+    // Call service layer to get doctor data
     const serviceResponse = await getDoctorService();
 
+    // Send standardized success response
     respond.success(res, serviceResponse);
-    
   } catch (err) {
     // pass error to global error handler
     next(err);
