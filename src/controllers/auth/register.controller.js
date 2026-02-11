ea4db01c477698d530d4d37248db3cac63e6a933
AshutoshPandey <ashutosh2239@gmail.com>
2026-02-11 21:56:18 +0530

    register controller improved

--- src/controllers/auth/register.controller.js
+++ src/controllers/auth/register.controller.js
@@ -1,24 +1,27 @@
 /**
  * Purpose:
  * Handles new user registration.
- *
+ 
  * When it is used:
- * Called when a user creates a new account using email and password.
- *
+ * Called when a user creates a new account using username  ,  email and password.
+ 
  * Access:
  * Public route.
  */
+
+
 import { registerService } from "../../services/auth/register.service.js";
 import { respond } from "../../utils/respond.js";
-import { HTTP_CODES } from "../../utils/httpCodes.js";
-import { AUTH_MESSAGES } from "../../utils/messages/auth.message.js";
 
 export const registerController = async (req, res, next) => {
   try {
-    const response  =  await registerService(req.body);
-    respond.success(res, response  );
+    // Call service layer to handle registration logic
+    const serviceResponse = await registerService(req.body);
+
+    // Send standardized success response
+    respond.success(res, serviceResponse);
   } catch (err) {
-    console.log("err register " , err);
+    // pass error to global error handler
     next(err);
   }
 };
