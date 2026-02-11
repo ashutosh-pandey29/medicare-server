675bd2a960f34e1c78966b7371193eb3bd930ff6
AshutoshPandey <ashutosh2239@gmail.com>
2026-02-11 23:24:48 +0530

    db connection file updated

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -3,10 +3,14 @@ import { env } from "./env.js";
 
 const connectDB = async () => {
   try {
+    // Connect to MongoDB using connection string from environment
     const conn = await mongoose.connect(env.MONGO_URI);
-    console.log("Database connected successfully:", conn.connection.host);
+
+    // console.log("Database connected successfully:", conn.connection.host);
   } catch (err) {
-    console.error("DB connecting failed:", err.message);
+    // console.error("DB connecting failed:", err.message);
+
+    // Stop application if database is not connected
     process.exit(1);
   }
 };
