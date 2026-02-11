7a7b93a04be94295e1f99b86f6930bf525e11f1f
AshutoshPandey <ashutosh2239@gmail.com>
2026-02-11 23:34:02 +0530

    comment added in passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,18 +1,32 @@
+// Import passport for authentication
 import passport from "passport";
+
+// Import Google OAuth strategy
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+// Import environment variables
 import { env } from "./env.js";
 
+// Configure Google authentication strategy
 passport.use(
   new GoogleStrategy(
     {
+      // Google App credentials
       clientID: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+
+      // URL where Google will redirect after login
       callbackURL: `${env.GOOGLE_CALLBACK_URL}/api/v1/auth/google/callback`,
     },
+
+    // This function runs after successful Google authentication
     function (accessToken, refreshToken, profile, cb) {
       try {
+        // profile contains user data from Google
+        // We are passing the profile to next step
         cb(null, profile);
       } catch (err) {
+        // If any error occurs, pass error
         return cb(err, null);
       }
     }
