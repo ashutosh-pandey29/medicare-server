b182ade5d14ed76af673d2df8dcdecdba7f1f174
AshutoshPandey <ashutosh2239@gmail.com>
2026-02-16 22:16:58 +0530

    notify.js updated

--- src/socket/notify.js
+++ src/socket/notify.js
@@ -1,10 +1,14 @@
+// Import the instance
 import { getIO } from "./init.socket.js";
+
+// SEND REAL-TIME NOTIFICATION
 export const notifyRealtime = async (userId, payload) => {
   try {
+    // Get initialized Socket.IO instance
     const io = getIO();
 
+    // Emit "notification" event to a specific user's room
     io.to(userId.toString()).emit("notification", payload);
-    console.log("notification sent to:", userId);
   } catch (err) {
     console.error("Socket notification failed:", err);
   }
