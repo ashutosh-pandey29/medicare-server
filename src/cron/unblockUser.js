43a2801d4ca3d9c537407d942adfc8c258644610
AshutoshPandey <ashutosh2239@gmail.com>
2026-03-19 09:23:24 +0530

    add robust time-based account unblock mechanism

--- src/cron/unblockUser.js
+++ src/cron/unblockUser.js
@@ -1,8 +1,10 @@
-
+import { cronService } from "../services/cron/cron.service";
 /**
  * Automatically unblock users blocked for more than 24h
  */
 
-export const unblockUser = async = () => {
-  
-}
+export const unblockUser = async () => {
+  const EXPIRE = 24; // 24 HR
+  const expiryDate = new Date();
+  await cronService.unblockUser(expiryDate);
+};
--- src/services/cron/cron.service.js
+++ src/services/cron/cron.service.js
@@ -1,6 +1,7 @@
 import User from "../../models/User.js";
 import { db } from "../../services/db/db.service.js";
 export const cronService = {
+  // clean unverified user
   cleanUnverifiedUser: async (expDate) => {
     const filter = {
       isEmailVerified: false,
@@ -8,5 +9,18 @@ export const cronService = {
     };
 
     await db.deleteMany(User, filter);
+  },
+
+  // unblock user
+  unblockUser: async (expDate) => {
+    const filter = {
+      isBlocked: true,
+      blockedUntil: { $lte: expDate },
+    };
+
+    await db.updateMany(User, filter, {
+      isBlocked: false,
+      blockedUntil: null,
+    });
   },
 };
